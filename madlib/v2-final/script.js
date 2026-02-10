9c203c45bfe6517aedc281d2953d6221af18de87
Emily-Mae <ermcconihe@ucdavis.edu>
2026-02-10 13:13:55 -0800

    fix

--- madlib/v2-final/script.js
+++ madlib/v2-final/script.js
@@ -7,7 +7,7 @@
     const questions = document.querySelector('#questions');
     const formData = document.querySelectorAll("input[type=text]");
     // const selectData = document.querySelector('select');
-    formData.push(selectData);
+    // formData.push(selectData);
 
     myForm.addEventListener('submit', function(event){
         event.preventDefault();
