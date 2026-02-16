4d77535452710b4724e9fb24c9bff0c68b676274
Emily-Mae <ermcconihe@ucdavis.edu>
2026-02-15 19:06:17 -0800

    improvements

--- madlib/improvements/script.js
+++ madlib/improvements/script.js
@@ -4,6 +4,8 @@
 
     const myForm = document.querySelector('#form');
     const madlib = document.querySelector('#madlib');
+    const output = document.querySelector('#output')
+    const again = document.querySelector("#again");
     const questions = document.querySelector('#questions');
     const formData = document.querySelectorAll("input[type=text]");
     // const selectData = document.querySelector('select');
@@ -42,9 +44,9 @@
     };
 
     function makeMadLib(words){
-        const myText = `You <span>${words[0]}</span> out of the grocery store, just have bagged up your groceries in a <span>${words[1]}</span> bag. It is a beautiful <span>${words[2]}</span> day outside and you get distracted watching a <span>${words[3]}</span> dart along in the distance. What you don’t see is the <span>${words[4]}</span> right in front of you, causing you to trip. Your bag <span>${words[5]}</span> out of your arms and <span>${words[6]}</span>. All <span>${words[7]}</span> of your <span>${words[8]}</span> go absolutely everywhere, causing a scene. Unfortunately for you, you are also on the ground and see the <span>${words[3]}</span> from before laughing at you.`;
+        const myText = `<p>You <span>${words[0]}</span> out of the grocery store, just have bagged up your groceries in a <span>${words[1]}</span> bag. It is a beautiful <span>${words[2]}</span> day outside and you get distracted watching a <span>${words[3]}</span> dart along in the distance. What you don’t see is the <span>${words[4]}</span> right in front of you, causing you to trip. Your bag <span>${words[5]}</span> out of your arms and <span>${words[6]}</span>. All <span>${words[7]}</span> of your <span>${words[8]}</span> go absolutely everywhere, causing a scene. Unfortunately for you, you are also on the ground and see the <span>${words[3]}</span> from before laughing at you.</p>`;
     
-        madlib.innerHTML = myText;
+        output.innerHTML = myText;
 
         madlib.style.display = "block";
         questions.style.display = "none";
@@ -52,9 +54,13 @@
         for( const eachField of formData){
             eachField.value = '';
         }
-        
 
     };
+
+    again.addEventListener('click', function(){
+        madlib.style.display = "none";
+        questions.style.display = "block";
+    });
 
 
 })();
